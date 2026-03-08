c3286fe61c43d3899ae5a694ca586cc76d3b7a30
pavellogvincev-commits <pavellogvincev@gmail.com>
2026-03-08 10:10:53 +0300

    Update Bullet.js

--- js/Bullet.js
+++ js/Bullet.js
@@ -1,19 +1,19 @@
 export class Bullet {
-    constructor(x, y, angle) {
+    constructor(x, y, angle, owner) {
         this.x = x;
         this.y = y;
         this.angle = angle;
+        this.owner = owner; // 'player' или 'enemy'
         
-        this.speed = 400; 
+        this.speed = 500; 
         this.radius = 3;  
         this.toDestroy = false; 
+        this.penetration = 80; // Бронепробитие
 
-        // Теперь мы храним скорость по осям X и Y для удобства рикошета
         this.vx = Math.cos(angle) * this.speed;
         this.vy = Math.sin(angle) * this.speed;
     }
 
-    // Добавили функцию spawnSparks, которую передадим из main.js
     update(dt, arena, spawnSparks) {
         this.x += this.vx * dt;
         this.y += this.vy * dt;
@@ -21,38 +21,25 @@ export class Bullet {
         let col = arena.checkBulletCollision(this.x, this.y, this.radius);
         
         if (col.hit) {
-            // Вычисляем угол падения (между вектором полета и нормалью стены)
-            // Нормализуем текущий вектор скорости
             let dirX = this.vx / this.speed;
             let dirY = this.vy / this.speed;
 
-            // Dot Product (Скалярное произведение)
             let dotProduct = -(dirX * col.nx + dirY * col.ny);
-            // Ограничиваем от -1 до 1 для безопасности математики
             dotProduct = Math.max(-1, Math.min(1, dotProduct)); 
             
-            // Получаем угол в градусах (0 - прямо в лоб, 90 - по касательной)
             let angleOfIncidence = Math.acos(dotProduct) * (180 / Math.PI);
 
-            if (angleOfIncidence >= 40) {
-                // РИКОШЕТ! Формула отражения вектора
+            if (angleOfIncidence >= 55) {
                 let dotV = this.vx * col.nx + this.vy * col.ny;
                 this.vx = this.vx - 2 * dotV * col.nx;
                 this.vy = this.vy - 2 * dotV * col.ny;
                 
-                // Обновляем визуальный угол снаряда
                 this.angle = Math.atan2(this.vy, this.vx);
-
-                // Чуть-чуть сдвигаем снаряд от стены, чтобы он не застрял внутри
                 this.x += col.nx * 2;
                 this.y += col.ny * 2;
             } else {
-                // ВЗРЫВ О СТЕНУ (Угол меньше 45 градусов)
                 this.toDestroy = true;
-                // Создаем искры, которые полетят в сторону нормали (от стены)
-                if (spawnSparks) {
-                    spawnSparks(this.x, this.y, col.nx, col.ny);
-                }
+                if (spawnSparks) spawnSparks(this.x, this.y, col.nx, col.ny);
             }
         }
     }
@@ -70,6 +57,3 @@ export class Bullet {
         ctx.restore();
     }
 }
-
-
-
