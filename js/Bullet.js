a36173351ffa7d8ad4722ce015aaa45768adf970
pavellogvincev-commits <pavellogvincev@gmail.com>
2026-03-09 22:12:06 +0300

    Update Bullet.js

--- js/Bullet.js
+++ js/Bullet.js
@@ -8,20 +8,18 @@ export class Bullet {
         this.radius = radius;
         this.color = color;
 
-        this.speed = 450; // СТРОГО 450
+        this.speed = 450; // СТРОГО 450 по твоей просьбе
         this.toDestroy = false;
 
         this.vx = Math.cos(angle) * this.speed;
         this.vy = Math.sin(angle) * this.speed;
 
-        // ВЕРНУЛИ ЛОГИКУ ЗАТУХАНИЯ
         this.isDecaying = false;
         this.maxDecayTime = 0.5;
         this.decayTimer = this.maxDecayTime;
     }
 
     update(dt, arena, spawnSparks, bounceCallback) {
-        // Если пуля уже отскочила и затухает
         if (this.isDecaying) {
             this.decayTimer -= dt;
             if (this.decayTimer <= 0) this.toDestroy = true;
@@ -36,18 +34,27 @@ export class Bullet {
         let hitWall = false;
         let nx = 0, ny = 0;
 
-        // ВЕРНУЛИ СТОЛКНОВЕНИЯ СО СТЕНАМИ АРЕНЫ
+        // 1. Проверяем ось X (Границы арены И препятствия внутри)
         if (nextX < 0) { hitWall = true; nx = 1; ny = 0; this.x = 0; }
         else if (nextX > arena.width) { hitWall = true; nx = -1; ny = 0; this.x = arena.width; }
-        else { this.x = nextX; }
+        else if (arena.checkCollision(nextX, this.y, this.radius)) { 
+            hitWall = true; nx = -Math.sign(this.vx); ny = 0; // Отскок от блока по X
+        } else { 
+            this.x = nextX; 
+        }
 
+        // 2. Проверяем ось Y (Границы арены И препятствия внутри)
         if (nextY < 0) { hitWall = true; nx = 0; ny = 1; this.y = 0; }
         else if (nextY > arena.height) { hitWall = true; nx = 0; ny = -1; this.y = arena.height; }
-        else { this.y = nextY; }
+        else if (arena.checkCollision(this.x, nextY, this.radius)) { 
+            hitWall = true; nx = 0; ny = -Math.sign(this.vy); // Отскок от блока по Y
+        } else { 
+            this.y = nextY; 
+        }
 
         if (hitWall) {
             this.bounce(nx, ny);
-            this.isDecaying = true; // Начинает исчезать
+            this.isDecaying = true; 
             if (spawnSparks) spawnSparks(this.x, this.y, nx, ny);
             if (bounceCallback) bounceCallback();
         }
@@ -65,16 +72,13 @@ export class Bullet {
         ctx.translate(this.x, this.y);
         ctx.rotate(this.angle);
 
-        // Прозрачность зависит от таймера затухания
         let alpha = this.isDecaying ? Math.max(0, this.decayTimer / this.maxDecayTime) : 1.0;
         
         ctx.fillStyle = this.color;
         ctx.globalAlpha = alpha * 0.5;
-        // След (трассер)
         ctx.fillRect(-this.radius * 6, -this.radius * 0.8, this.radius * 6, this.radius * 1.6);
         
         ctx.globalAlpha = alpha;
-        // Сама пуля
         ctx.beginPath();
         ctx.arc(0, 0, this.radius, 0, Math.PI * 2);
         ctx.fill();
