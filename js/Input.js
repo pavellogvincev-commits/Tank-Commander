ce7db9a1ace44270f1abf2c2e9ad0b19fb0542d9
pavellogvincev-commits <pavellogvincev@gmail.com>
2026-03-09 18:38:13 +0300

    Update Input.js

--- js/Input.js
+++ js/Input.js
@@ -3,32 +3,43 @@ export class Input {
         this.keys = {};
         this.mouseX = 0;
         this.mouseY = 0;
-        this.mouseDown = false; // <-- НОВОЕ
+        this.shooting = false;
 
-        window.addEventListener('keydown', (e) => this.keys[e.code] = true);
-        window.addEventListener('keyup', (e) => this.keys[e.code] = false);
+        // Отслеживаем нажатия клавиатуры
+        window.addEventListener('keydown', (e) => {
+            this.keys[e.code] = true;
+        });
 
+        window.addEventListener('keyup', (e) => {
+            this.keys[e.code] = false;
+        });
+
+        // Отслеживаем мышь для поворота башни
         canvas.addEventListener('mousemove', (e) => {
             const rect = canvas.getBoundingClientRect();
             this.mouseX = e.clientX - rect.left;
             this.mouseY = e.clientY - rect.top;
         });
 
-        // --- НОВОЕ: Обработка мыши ---
+        // Отслеживаем клик мыши для стрельбы
         canvas.addEventListener('mousedown', (e) => {
-            if (e.button === 0) this.mouseDown = true; // 0 - левая кнопка
+            if (e.button === 0) this.shooting = true; // Левая кнопка мыши
         });
 
         canvas.addEventListener('mouseup', (e) => {
-            if (e.button === 0) this.mouseDown = false;
+            if (e.button === 0) this.shooting = false;
         });
     }
 
-    isForward() { return this.keys['KeyW'] || this.keys['ArrowUp']; }
-    isBackward() { return this.keys['KeyS'] || this.keys['ArrowDown']; }
+    // Методы, которые запрашивает Tank.js
+    isUp() { return this.keys['KeyW'] || this.keys['ArrowUp']; }
+    isDown() { return this.keys['KeyS'] || this.keys['ArrowDown']; }
     isLeft() { return this.keys['KeyA'] || this.keys['ArrowLeft']; }
     isRight() { return this.keys['KeyD'] || this.keys['ArrowRight']; }
     
-    // --- НОВОЕ: Проверка стрельбы ---
-    isShooting() { return this.mouseDown; }
+    // Стрелять можно левым кликом ИЛИ пробелом
+    isShooting() { return this.shooting || this.keys['Space']; }
+    
+    getMouseX() { return this.mouseX; }
+    getMouseY() { return this.mouseY; }
 }
